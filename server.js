7a936a0a99ec57c48bdb734e0eb7c4e85339d203
matze45jub <elke-matthias@gmx.net>
2025-02-24 11:01:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
+
+// Statische Dateien servieren
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route für die Hauptseite
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'bedienung.html'));
+});
 
 // Globales Objekt zur Speicherung aller Bestellungen
 let globalOrders = {};
